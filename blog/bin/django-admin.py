45cdcc6d85f2e57bf3a3839fb98bfbdabf4e2282
Jumania <jumania2000@outlook.com>
2016-12-06 12:03:39 +0300

    My Django add, first commit

---
+++ blog/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/django1/dj_test18/blog/bin/python2.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ blog/lib/python2.7/copy_reg.py
@@ -0,0 +1,1 @@
+/home/django1/.pyenv/versions/2.7.11/lib/python2.7/copy_reg.py
